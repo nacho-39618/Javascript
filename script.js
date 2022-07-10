ea9ed67b83ac1dfc340dea90ac47fd7a34af7560
nacho-39618 <104328032+nacho-39618@users.noreply.github.com>
2022-07-09 21:02:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,35 @@
 
-/*let numero1,numero2,operacion
-do{
-    numero1 = parseFloat(prompt("Ingrese año de nacimiento"))
-    numero2 = 2022
-    operacion = prompt("Ingrese una operacion(+, - ,*, /)")
-} while(isNaN(numero1) || isNaN(numero2))
+let numero1
+let CantidadDeCuotas 
+const interesen3cuotas = 1.3
+const interesen6cuotas = 1.4
 
-switch(operacion){
-    case "+":
-    alert(numero1 + numero2);
-    break;
+numero1 = parseFloat(prompt("Ingrese precio del producto"))
 
-    case "-":
-        alert(numero1 - numero2);
-        break;
-
-    case "*":
-        alert(numero1 * numero2);
-        break;
-
-    case "/":
-        alert(numero1 / numero2);
-        break;
-    
-    default:
-        alert("operacion no valida");
-        break;
+while(isNaN(numero1) === true){
+    alert("Ingrese Numero valido");
+    numero1 = parseFloat(prompt("Ingrese precio del producto"))
 
 }
-*/
+CantidadDeCuotas = parseFloat(prompt("Ingrese 1, 3 o 6 cuotas"))
 
-let numero1 = parseFloat(prompt("ingrese año de nacimiento y te diremos tu edad"))
-let numero2 = 2022
+while(CantidadDeCuotas !==  1 && CantidadDeCuotas !== 3 && CantidadDeCuotas !== 6) {
+    alert("El mercado no dispone de esa cantidad de cuotas")
+    CantidadDeCuotas = parseFloat(prompt("Ingrese 1, 3 o 6 cuotas"))
+}
 
-if(isNaN(numero1))
- alert("Numero no valido")
- 
- else{
-     alert(numero2 - numero1)
- }
+function preciofinal(numero1, CantidadDeCuotas){
+    if(CantidadDeCuotas === 1){
+        return alert("usted va a pagar una cuota de " + numero1) ;
+    }
+        else if(CantidadDeCuotas === 3 ) {
+            return alert("Usted va a pagar un total de " + numero1 * interesen3cuotas + " en  3 cuotas de " + numero1 * interesen3cuotas / 3 );
+        }
+        else{
+            return alert("Usted va a pagar 6 cuotas de " + numero1 * interesen6cuotas + " en 6 cuotas de " + numero1 * interesen6cuotas / 6);
+        }
+}
+console.log(preciofinal(numero1, CantidadDeCuotas))
+
+
+
